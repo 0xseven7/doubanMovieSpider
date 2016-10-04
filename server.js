ddc51c71a37a1838418aa54286bcbf4fca719359
zhangchenxu <1161888512@qq.com>
2016-10-04 21:39:41 +0800

    运行的时候会被终止，还是需要添加异步。但是现在先试试保存到文件

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ var ep = new eventproxy();
 var mainUrl = 'https://movie.douban.com/j/search_tags?type=movie';
 var movies = [];
 var tagUrls = [];
-var nowTagUrls = [];
 
 
 function start() {
@@ -29,45 +28,39 @@ function start() {
 }
 function tagUrlStart(req, res, movieTags) {
     var page_limit = 0,
-        page_start = 20;
-
-        movieTags.forEach(function (movietag) {
-        tagUrls.push('https://movie.douban.com/j/search_subjects?type=movie&tag=' + encodeURI(movietag) + // 这里讲中文转为encode，否则无法抓取
+        page_start = 0,
+        crowlCount = 0;
+    movieTags.forEach(function (movietag) {
+        tagUrls.push('https://movie.douban.com/j/search_subjects?type=movie&tag=' + encodeURI(movietag)+ // 这里讲中文转为encode，否则无法抓取
             '&sort=recommend&page_limit=20&page_start=');
     })
-    for(var i=0; i<50; i++){
-        var nowTagUrl = tagUrls[0];
-        nowTagUrls.push(nowTagUrl + page_start);
-        page_start+=20;
-    }
     res.writeHead(200, {'Content-Type':'text/html;charset="utf-8"'})
-    var crawlCount = 0;
-    var count = 0;
-    function crawl(url, callback) {
-        crawlCount++;
-        var delay = parseInt(Math.random() * 5000, 10)
-        console.log("正在执行第" + crawlCount + "次爬取" +"正在抓取的是" + url + "延时" + delay + "毫秒")
-        superagent.get(url).end(function (err, pres) {
-            if(!pres.text){
-                count
+    var tagIndex = 0;
+    var timer = setInterval(crawl, 100);
+
+    function crawl() {
+        var crawlUrl = tagUrls[tagIndex];
+        page_start += 20;
+
+        crawlUrl += page_start;
+        console.log("正在执行第" + crowlCount + "次爬取" + "共" + crawlUrl + "条信息")
+        superagent.get(crawlUrl).end(function (err, pres) {
+            console.log()
+            if(!JSON.parse(pres.text).subjects[0]){
+                clearInterval(timer);
+                tagIndex++;
+                timer = setInterval(crawl, 1000)
+                page_start = 0;
+            } else {
+                JSON.parse(pres.text).subjects.forEach(function (subject) {
+                    res.write(movieTags[tagIndex] + crowlCount + subject.title + subject.rate + "</br>");
+                    crowlCount++;
+                })
             }
-            JSON.parse(pres.text).subjects.forEach(function (subject) {
-                res.write(count + subject.title + subject.rate  + "</br>");
-            })
+
         })
-        setTimeout(function () {
-            crawlCount--;
-            callback(null, url + "call back content")
-        }, delay)
     }
-    async.mapLimit(nowTagUrls, 2, function (url, callback) {
-        crawl(url, callback);
-    }, function (err, result) {
-        console.log('final' + result);
-    })
 }
-
-
 start();
 
 
